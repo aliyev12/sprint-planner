7237af081c84179ceea8c9f8b3778e58e0871bc8
Abdul Aliyev <aaa7c4@gmail.com>
2020-07-12 14:33:19 -0400

    Add checks next to users that voted

--- client/src/common/utils.ts
+++ client/src/common/utils.ts
@@ -15,3 +15,24 @@ export const categoryActive = (
     return true;
   return false;
 };
+
+export const truncate = (
+  str: string,
+  len: number = 14,
+  dots: boolean = false
+): string => {
+  if (str.length > len && str.length > 0) {
+    let new_str = str + " ";
+    new_str = str.substr(0, len);
+    new_str = str.substr(0, new_str.lastIndexOf(" "));
+    new_str = new_str.length > 0 ? new_str : str.substr(0, len);
+    new_str =
+      str.split(" ")[0].length > len
+        ? str.split(" ")[0].substr(0, len - 3) + "..."
+        : new_str;
+    new_str =
+      str.split(" ")[0].length <= len && dots ? new_str + "..." : new_str;
+    return new_str;
+  }
+  return str;
+};
