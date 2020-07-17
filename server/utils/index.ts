05c6cd731e57c3a766dbcc5626d65c8b2fb2e48c
Abdul Aliyev <aaa7c4@gmail.com>
2020-07-16 23:47:04 -0500

    Add username to current user to navbar

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -1,5 +1,6 @@
 export * from "./messages";
 export * from "./constants";
+export * from "./toMilliseconds";
 
 export const isNumOrFloat = (num: string | number) =>
   /^[+-]?\d+(\.\d+)?$/g.test(num.toString());
